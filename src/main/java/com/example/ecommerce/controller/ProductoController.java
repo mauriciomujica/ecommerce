11d81fc4c68c775a72484254df5fbe89ddafd686
mau <mauricio.mujica@hotmail.com>
2026-05-28 20:50:28 -0300

    Añadida la función para buscar productos con su nombre usando el parametro 'nombre'

--- src/main/java/com/example/ecommerce/controller/ProductoController.java
+++ src/main/java/com/example/ecommerce/controller/ProductoController.java
@@ -2,35 +2,46 @@ package com.example.ecommerce.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.ecommerce.dao.ProductoRepository;
 import com.example.ecommerce.model.Producto;
 import com.example.ecommerce.service.ProductoService;
+
+import io.micrometer.common.util.StringUtils;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-
-
-
 
 @RestController
 @RequestMapping("/productos")
 public class ProductoController {
     private final ProductoService service;
 
+    @Autowired
+    private ProductoRepository repo;
+
     public ProductoController(ProductoService service) {
         this.service = service;
     }
 
+
     @GetMapping()
-    public List<Producto> listarTodos() {
-        return service.listarTodos();
+    public List<Producto> obtenerProductos(@RequestParam(required=false) String nombre){
+        if (nombre == null || StringUtils.isEmpty(nombre)){
+            return service.listarTodos();
+        }
+        else return repo.findBynombreContaining(nombre);
+
     }
-    
+
     @GetMapping("/{id}")
     public Producto obtenerPorID(@PathVariable int id) {
         return service.obtenerPorID(id);
